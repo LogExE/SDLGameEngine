33e2058d19642fa7c2b9a364de28ad79479f0584
LogExE <awesome149712@gmail.com>
2023-05-26 14:27:35 +0400

    simplify

--- include/core/Graphics.hpp
+++ include/core/Graphics.hpp
@@ -4,40 +4,33 @@
 #include <string>
 #include <tuple>
 
+#include <SDL2/SDL.h>
+
 #include "primitives/Vector2.hpp"
 #include "primitives/Color.hpp"
 #include "primitives/Line.hpp"
 
 class Graphics
 {
-protected:
+private:
+    SDL_Renderer *m_rendr;
     bool m_vsync;
     Color m_draw_col;
 public:
-    Graphics() : m_draw_col(Color::black), m_vsync(false) {}
-    virtual ~Graphics() {}
+    Graphics(SDL_Window *wnd);
+    ~Graphics();
 
-    void toggle_vsync()
-    {
-        set_vsync(m_vsync ^= 1);
-    }
-    virtual void set_vsync(bool vsync)
-    {
-        m_vsync = vsync;
-    }
-    bool get_vsync()
-    {
-        return m_vsync;
-    }
-    virtual void set_draw_color(Color color)
-    {
-        m_draw_col = color;
-    }
+    void toggle_vsync();
+    void set_vsync(bool vsync);
+    bool get_vsync();
+    void set_draw_color(Color color);
 
-    virtual void draw_line(Line line) = 0;
-    virtual void draw_text(const std::string &msg, Vector2 pos) = 0;
+    
+
+    void draw_line(Line line);
+    void draw_text(const std::string &msg, Vector2 pos);
     //virtual void draw_sprite() = 0;
 
-    virtual void present() = 0;
-    virtual void clear(Color cl_color = Color::white) = 0;
+    void present();
+    void clear(Color cl_color = Color::white);
 };
